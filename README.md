735ed4a6fca4850a79c721ad6d58e754feb1caed
sine-b <ethandamdragon@gmail.com>
2023-01-11 12:57:22 -0500

    setup express backend

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# meltwater-assessment
+**These apps were built using Node v17.9.1**
+
+---
+
+## Running the backend
+
+Inside the `server/` directory, run `npm i` then `npm start`. If successful you should see the message:
+
+```
+Server is now listening on port: 8080
+```
---
+++ server/router.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const redact = require('./redact');
+
+const errorObj = Object.freeze({
+  statusText: 'failure',
+  message: 'You must enter a list of words or phrases to redact.',
+});
+
+const router = express.Router();
+
+router.use((_, _, next) => {
+  setTimeout(next, 500);
+});
+
+router.get('/', (_, response) => {
+  response.json({ statusText: 'success' });
+});
+
+router.post('/', (request, response) => {
+  const { file, body } = request;
+
+  if (!file) {
+    return response.status(400).json(errorObj);
+  }
+  if (!body.banText) {
+    return response.status(400).json(errorObj);
+  }
+
+  const censoredDocument = redact(body.banText, file.path);
+
+  response.json({ statusText: 'success', document: censoredDocument });
+});
+
+module.exports = router;
